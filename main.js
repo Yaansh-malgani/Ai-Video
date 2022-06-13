e77b3c0e06e2292a4a4868c13bc37543cba183c6
Yaansh-malgani <82943874+Yaansh-malgani@users.noreply.github.com>
2022-06-13 16:43:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+video = "";
+var Status = "";
+
+function preload() {
+    video = createVideo('video.mp4');
+    video.hide();
+}
+
+function setup() {
+    canvas = createCanvas(480, 380);
+    canvas.center();
+}
+
+function start() {
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function draw() {
+    image(video, 0, 0, 480, 380);
+}
+
+function modelLoaded() {
+    console.log("Model Loaded !");
+    Status = true;
+    video.loop();
+    video.speed(1);
+    video.volume(0);
+}
